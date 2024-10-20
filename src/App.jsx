705b06fa0c35a438a04b6027ddb32ee56c2c98fc
NexxCod <drmsalinasv@gmail.com>
2024-10-20 14:48:51 -0300

    Arreglo CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import './App.css'
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
